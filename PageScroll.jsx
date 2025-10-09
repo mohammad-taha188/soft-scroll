6b9168d012ef34ab57fba9ce4c6a5898a1a98e1d
mohammad-taha188 <mmd.programmer8@gmail.com>
2025-10-09 10:50:12 +0330

    Update PageScroll.jsx

--- PageScroll.jsx
+++ PageScroll.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from "react";
 
-export default function PageScroll({ children }) {
+export default function PageScroll({ children, opacity }) {
   const [current, setCurrent] = useState(0);
   const [direction, setDirection] = useState("down");
   const isScrolling = useRef(false); // Flag برای جلوگیری از چند Scroll پشت سر هم
@@ -98,40 +98,42 @@ export default function PageScroll({ children }) {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* Sections */}
-      {children.map((child, i) => {
-        const style = {
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          transition: "transform 1.1s ease, opacity 1.1s ease",
-          transform:
-            i === current
-              ? "translateY(0)"
-              : i < current
-              ? "translateY(-100%)"
-              : "translateY(100%)",
-          opacity: i === current ? 1 : 0.4,
-        };
-        return (
-          <div key={i} style={style}>
-            {child}
-          </div>
-        );
-      })}
+      {children &&
+        children?.map((child, i) => {
+          const style = {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            transition: "transform 1.1s ease, opacity 1.1s ease",
+            transform:
+              i === current
+                ? "translateY(0)"
+                : i < current
+                ? "translateY(-100%)"
+                : "translateY(100%)",
+            opacity: i === current ? 1 : opacity,
+          };
+          return (
+            <div key={i} style={style}>
+              {child}
+            </div>
+          );
+        })}
 
       {/* Dots */}
-      <div className="absolute right-6 top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-50 border-gray-200 shadow shadow-gray-100 border p-2 rounded-sm bg-white">
-        {children.map((_, i) => (
-          <button
-            key={i}
-            onClick={() => goTo(i)}
-            className={`w-2 h-2 rounded-full transition-all duration-300 cursor-pointer ${
-              i === current ? "bg-gray-700 scale-125" : "bg-gray-400"
-            }`}
-          />
-        ))}
+      <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-50 border-gray-200 shadow shadow-gray-100 border p-2 rounded-sm bg-white">
+        {children &&
+          children?.map((_, i) => (
+            <button
+              key={i}
+              onClick={() => goTo(i)}
+              className={`w-2 h-2 rounded-full transition-all duration-300 cursor-pointer ${
+                i === current ? "bg-gray-700 scale-125" : "bg-gray-400"
+              }`}
+            />
+          ))}
       </div>
     </div>
   );
